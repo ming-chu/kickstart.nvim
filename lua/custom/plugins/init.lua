2b189ad9e5027f6e641c58fdb0a38edd578269d1
Kenneth <kennth.chu@gmail.com>
2024-08-08 20:25:24 -0400

    --wip-- [skip ci]

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,8 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  { 'Mofiqul/dracula.nvim' },
+
   {
     'kylechui/nvim-surround',
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
