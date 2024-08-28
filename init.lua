05abaa2f5d0f69f969dc442b92b427bcd7910831
Kenneth Chu <kennth.chu@gmail.com>
2024-08-28 09:03:55 -0400

    Try darcula-dark

--- init.lua
+++ init.lua
@@ -831,28 +831,39 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-night'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none guifg=#808080'
+  --     vim.cmd.hi 'Visual guibg=#999900 guifg=#000000 gui=none'
+  --
+  --     -- color for relative line numbers
+  --     vim.cmd.hi 'LineNrAbove guibg=none guifg=grey'
+  --     vim.cmd.hi 'LineNrBelow guibg=none guifg=grey'
+  --     vim.cmd.hi 'CursorLineNr guifg=orange' -- with cursorline enable this will be the color of the current line number
+  --     -- vim.cmd.hi 'LineNr guifg=orange'
+  --   end,
+  -- },
+
+  {
+    'xiantang/darcula-dark.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none guifg=#808080'
-      vim.cmd.hi 'Visual guibg=#999900 guifg=#000000 gui=none'
-
-      -- color for relative line numbers
-      vim.cmd.hi 'LineNrAbove guibg=none guifg=grey'
-      vim.cmd.hi 'LineNrBelow guibg=none guifg=grey'
+      vim.cmd 'colorscheme darcula-dark'
       vim.cmd.hi 'CursorLineNr guifg=orange' -- with cursorline enable this will be the color of the current line number
-      -- vim.cmd.hi 'LineNr guifg=orange'
     end,
   },
 
