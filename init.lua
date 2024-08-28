7b23770db03cca43d4d4b75c3fd99f2f5c475e22
Kenneth <kennth.chu@gmail.com>
2024-08-28 10:21:33 -0400

    Add fold nvim-ufo

--- init.lua
+++ init.lua
@@ -867,6 +867,34 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      'kevinhwang91/promise-async',
+    },
+    init = function()
+      vim.o.foldcolumn = '1' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    end,
+    config = function()
+      -- Option 3: treesitter as a main provider instead
+      -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+      -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+      -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+      require('ufo').setup {
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      }
+    end,
+  },
+
   -- {
   --   priority = 1000, -- Make sure to load this before all the other start plugins.
   --   'Mofiqul/dracula.nvim',
